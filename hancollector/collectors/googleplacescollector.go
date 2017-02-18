af5aad6ddf7ad592193186eda34c73d91f38987e
Oliver <oliveroneill04@gmail.com>
2017-02-18 17:25:47 +1100

    fixed: bug in google places only updating once a day

--- hancollector/collectors/googleplacescollector.go
+++ hancollector/collectors/googleplacescollector.go
@@ -3,10 +3,7 @@ package collectors
 import (
     "fmt"
     "time"
-<<<<<<< HEAD
     "regexp"
-=======
->>>>>>> added: google places collector
     "googlemaps.github.io/maps"
     "golang.org/x/net/context"
     "github.com/kellydunn/golang-geo"
@@ -28,16 +25,11 @@ func NewGooglePlacesCollector() *GooglePlacesCollector {
 
 // GetConfig returns the configuration for the Google Places source
 // Use this to store api keys and enable/disable collectors
-<<<<<<< HEAD
 func (c *GooglePlacesCollector) GetConfig() config.CollectorConfiguration {
-=======
-func (c GooglePlacesCollector) GetConfig() config.CollectorConfiguration {
->>>>>>> added: google places collector
     return config.GooglePlacesConfig
 }
 
 // GetImages returns new images queried by location on Google Places
-<<<<<<< HEAD
 func (c *GooglePlacesCollector) GetImages(lat float64, lng float64) ([]imagedata.ImageData, error) {
     if !c.GetConfig().IsEnabled() {
         return []imagedata.ImageData{}, nil
@@ -46,15 +38,6 @@ func (c *GooglePlacesCollector) GetImages(lat float64, lng float64) ([]imagedata
     // due to Google Maps strict query limits, we'll only query every 12 hours
     // Google Places updates very slowly anyway, so this should be fine
     if timeSinceLastUpdate < 12 * 60 * 60 && timeSinceLastUpdate > 1 {
-=======
-func (c GooglePlacesCollector) GetImages(lat float64, lng float64) ([]imagedata.ImageData, error) {
-    if !c.GetConfig().IsEnabled() {
-        return []imagedata.ImageData{}, nil
-    }
-    // due to Google Maps strict query limits, we'll only query every 12 hours
-    // Google Places updates very slowly anyway, so this should be fine
-    if time.Now().Unix() - c.timeSinceLastQuery < 12 * 60 * 60 {
->>>>>>> added: google places collector
         return []imagedata.ImageData{}, nil
     }
     c.timeSinceLastQuery = time.Now().Unix()
@@ -65,11 +48,7 @@ func (c GooglePlacesCollector) GetImages(lat float64, lng float64) ([]imagedata.
     return c.getImagesWithClient(client, lat, lng)
 }
 
-<<<<<<< HEAD
 func (c *GooglePlacesCollector) getImagesWithClient(client *maps.Client, lat float64, lng float64) ([]imagedata.ImageData, error) {
-=======
-func (c GooglePlacesCollector) getImagesWithClient(client *maps.Client, lat float64, lng float64) ([]imagedata.ImageData, error) {
->>>>>>> added: google places collector
     images, err := c.queryImages(client, lat, lng)
     if err != nil {
         return images, err
@@ -89,11 +68,7 @@ func (c GooglePlacesCollector) getImagesWithClient(client *maps.Client, lat floa
     return images, nil
 }
 
-<<<<<<< HEAD
 func (c *GooglePlacesCollector) queryImages(client *maps.Client, lat float64, lng float64) ([]imagedata.ImageData, error) {
-=======
-func (c GooglePlacesCollector) queryImages(client *maps.Client, lat float64, lng float64) ([]imagedata.ImageData, error) {
->>>>>>> added: google places collector
     r := &maps.NearbySearchRequest{
         Location: &maps.LatLng{lat, lng},
         Radius:   QueryRange,
@@ -107,7 +82,6 @@ func (c GooglePlacesCollector) queryImages(client *maps.Client, lat float64, lng
 
     images := []imagedata.ImageData {}
     for _, m := range media.Results {
-<<<<<<< HEAD
         if len(m.Photos) == 0 {
             continue
         }
@@ -150,19 +124,6 @@ func (c GooglePlacesCollector) queryImages(client *maps.Client, lat float64, lng
             m.Geometry.Location.Lat, m.Geometry.Location.Lng,
             link, userName, "", config.GooglePlacesConfig.CollectorName)
         images = append(images, *newImage)
-=======
-        for _, image := range m.Photos {
-            // format the url to include the photo reference
-            // use config/googleplaces.go to set up where the URL should point to
-            url := fmt.Sprintf("%s?photoreference=%s", config.GooglePlacesConfig.PhotoURL, image.PhotoReference)
-            // TODO: using the current timestamp may give a bias to google places
-            newImage := imagedata.NewImage(m.Name, time.Now().Unix(),
-                url, url, image.PhotoReference, m.Geometry.Location.Lat,
-                m.Geometry.Location.Lng, "", "", "",
-                config.GooglePlacesConfig.CollectorName)
-            images = append(images, *newImage)
-        }
->>>>>>> added: google places collector
     }
     return images, nil
 }
