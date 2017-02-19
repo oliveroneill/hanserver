1e055ee29720d2e0055e6ae2851fc5c50308cad7
Oliver <oliveroneill04@gmail.com>
2017-02-19 20:44:16 +1100

    updated: google places collector with error reporting functionality

--- hancollector/collectors/config/googleplaces.go
+++ hancollector/collectors/config/googleplaces.go
@@ -34,8 +34,10 @@ func (c GooglePlacesConfiguration) IsEnabled() bool {
 // GetCollectorName returns the name of the collector for logging purposes
 func (c GooglePlacesConfiguration) GetCollectorName() string {
     return c.CollectorName
-}
-// GetCollectorName returns the name of the collector for logging purposes
+}// GetCollectorName returns the name of the collector for logging purposes
+func (c GooglePlacesConfiguration) GetCollectorName() string {
+    return c.CollectorName
+}// GetCollectorName returns the name of the collector for logging purposes
 func (c GooglePlacesConfiguration) GetCollectorName() string {
     return c.CollectorName
 }// GetCollectorName returns the name of the collector for logging purposes
