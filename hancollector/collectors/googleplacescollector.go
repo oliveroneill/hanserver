ee6f75621a19c806cf2ad5fb25e8ac69373afae8
Oliver <oliveroneill04@gmail.com>
2017-02-19 17:35:05 +1100

    fixed: issues in duplicate google places images

--- hancollector/collectors/googleplacescollector.go
+++ hancollector/collectors/googleplacescollector.go
@@ -3,6 +3,7 @@ package collectors
 import (
     "fmt"
     "time"
+    "regexp"
     "googlemaps.github.io/maps"
     "golang.org/x/net/context"
     "github.com/kellydunn/golang-geo"
@@ -81,17 +82,48 @@ func (c *GooglePlacesCollector) queryImages(client *maps.Client, lat float64, ln
 
     images := []imagedata.ImageData {}
     for _, m := range media.Results {
-        for _, image := range m.Photos {
-            // format the url to include the photo reference
-            // use config/googleplaces.go to set up where the URL should point to
-            url := fmt.Sprintf("%s?photoreference=%s", config.GooglePlacesConfig.PhotoURL, image.PhotoReference)
-            // TODO: using the current timestamp may give a bias to google places
-            newImage := imagedata.NewImage(m.Name, time.Now().Unix(),
-                url, url, image.PhotoReference, m.Geometry.Location.Lat,
-                m.Geometry.Location.Lng, "", "", "",
-                config.GooglePlacesConfig.CollectorName)
-            images = append(images, *newImage)
+        if len(m.Photos) == 0 {
+            continue
         }
+        // Nearby Search only returns one image
+        // To retrieve more you can use a Place Details request, but it appears
+        // that there are no unique identifiers for images and different
+        // photo_reference values can point to the same image
+        image := m.Photos[0]
+        userName := ""
+        link := ""
+        // retrieve user name and link for attributions within the app
+        if len(image.HTMLAttributions) > 0 {
+            contrib := image.HTMLAttributions[0]
+            r, err := regexp.Compile(">(.*)<")
+            if err == nil {
+                results := r.FindStringSubmatch(contrib)
+                if len(results) > 1 {
+                    userName = results[1]
+                }
+            }
+            r, err = regexp.Compile("href=\"(.*)\"")
+            if err == nil {
+                results := r.FindStringSubmatch(contrib)
+                if len(results) > 1 {
+                    link = results[1]
+                }
+            }
+        }
+        // format the url to include the photo reference
+        // use `config/googleplaces.go` to set up where the URL should point to
+        url := fmt.Sprintf("http://%s?photoreference=%s", config.GooglePlacesConfig.PhotoURL, image.PhotoReference)
+        thumbnailURL := fmt.Sprintf("%s&maxwidth=%d", url, 200)
+        // choose maximum val for standard res
+        standardURL := fmt.Sprintf("%s&maxwidth=%d", url, 1599)
+        // For now the place name is used for the identifier to avoid duplicates
+        id := m.Name
+        // TODO: using the current timestamp may give a bias to google places
+        newImage := imagedata.NewImage(m.Name, time.Now().Unix(),
+            standardURL, thumbnailURL, id,
+            m.Geometry.Location.Lat, m.Geometry.Location.Lng,
+            link, userName, "", config.GooglePlacesConfig.CollectorName)
+        images = append(images, *newImage)
     }
     return images, nil
 }
